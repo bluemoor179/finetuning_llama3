b7cd45456b6a531d96cb4c968ac23fe6a11a4cdc
bluemoor179 <bluemoor179@gmail.com>
2024-09-09 16:42:11 +0900

    save_steps okay

--- main_3.py
+++ main_3.py
@@ -115,12 +115,12 @@ print('dataset load')
 from trl import SFTTrainer, SFTConfig
 
 sft_config = SFTConfig(
-    output_dir="./results2",
-    num_train_epochs=15, # Total number of training epochs to perform (if not an integer, will perform the decimal part percents of the last epoch before stopping training).
+    output_dir="./results",
+    num_train_epochs=10, # Total number of training epochs to perform (if not an integer, will perform the decimal part percents of the last epoch before stopping training).
     per_device_train_batch_size=4, # The batch size per GPU/XPU/TPU/MPS/NPU core/CPU for training.
     gradient_accumulation_steps=1, # Number of updates steps to accumulate the gradients for, before performing a backward/update pass.
     optim="paged_adamw_32bit", #The optimizer to use: adamw_hf, adamw_torch, adamw_torch_fused, adamw_apex_fused, adamw_anyprecision or adafactor.
-    save_steps=25, # Number of updates steps before two checkpoint saves if `save_strategy="steps"`. Should be an integer or a float in range `[0,1)`. If smaller than 1, will be interpreted as ratio of total training steps.
+    save_steps=2500, # Number of updates steps before two checkpoint saves if `save_strategy="steps"`. Should be an integer or a float in range `[0,1)`. If smaller than 1, will be interpreted as ratio of total training steps.
     logging_steps=25, # Number of update steps between two logs if `logging_strategy="steps"`. Should be an integer or a float in range `[0,1)`. If smaller than 1, will be interpreted as ratio of total training steps.
     learning_rate=2e-4, # The initial learning rate for [`AdamW`] optimizer.
     weight_decay=0.001, # The weight decay to apply (if not zero) to all layers except all bias and LayerNorm weights in [`AdamW`] optimizer.
@@ -140,9 +140,9 @@ sft_config = SFTConfig(
 trainer = SFTTrainer(
     model=model,
     # train_dataset=dataset.select([0, 10, 20, 30, 40, 50]),
-    train_dataset=dataset.select([0, 1, 2, 3, 4, 5]),
+    # train_dataset=dataset.select([0, 1, 2, 3, 4, 5]),
     # train_dataset=dataset.select([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]),
-    # train_dataset=dataset,
+    train_dataset=dataset,
     args=sft_config,
     peft_config=peft_params,
     tokenizer=tokenizer,
